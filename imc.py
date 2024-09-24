228a88e6f4d0b8a9c4215d5eda5bd6efd0643298
Germanir Leal <germanirleal@yahoo.com.br>
2024-09-23 21:25:47 -0300

    Final

--- imc.py
+++ imc.py
@@ -11,3 +11,20 @@ print("Vamos calcular seu IMC")
 imc = (peso / (altura * altura))
 
 print(pergunta_nome , (f"seu IMC é: {imc:.2f}"))
+
+if imc < 16:
+	print("Seu estado é de Magreza grave\nProcure um médico ou nutricionista.")
+elif imc < 17:
+	print("Seu estado é de Magreza moderada\nPrecisa rever sua alimentação.")
+elif imc < 18.5:
+	print("Seu estado é de Magreza leve\nPrecisa rever sua alimentação.")
+elif imc < 25:
+	print("Você está Saudável.\nParabéns!")
+elif imc < 30:
+	print("Seu estado é de Sobrepeso\nPrecisa rever sua alimentação.")
+elif imc < 35:
+	print("Seu estado é de Obesidade Grau I\nProcure um médico ou nutricionista.")
+elif imc < 40:
+	print("Seu estado é de Obesidade Grau II (severa)\nProcure um médico ou nutricionista.")
+else:
+	print("Seu estado é de Obesidade Grau III (mórbida)\nProcure um médico ou nutricionista.")
