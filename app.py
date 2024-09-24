97311fe73359128c517b1030842f2bf38c9f1d65
Germanir Leal <germanirleal@yahoo.com.br>
2024-09-23 21:18:05 -0300

    Versão 1

---
+++ app.py
@@ -0,0 +1,13 @@
+#Teste programa Python - Atividade 01
+
+nome = input("Digite seu nome: ")
+cpf = input ("Digite seu CPF ")
+telefone = input("Informe seu telefone ")
+email = input("Informe seu e-mail ")
+endereco = input("Informe seu endereço ")
+genero = input("Informe seu Gênero ")
+escolaridade = input("Informe sua escolaridade ")
+signo = input("Informe seu Signo ")
+tipo_sanguineo = input("Informe seu Tipo Sanguíneo ")
+
+print(f"Bem-vindo(a) {nome},  seus dados são: CPF: {cpf},  Telefone: {telefone},  E-mail: {email}, Gênero: {genero},  Escolaridade: {escolaridade},  Signo: {signo}, Tipo Sanguíneo: {tipo_sanguineo}")
